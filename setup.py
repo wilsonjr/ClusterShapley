50be3b6999dcf92002e033411e248d627b7499db
Wilson Estecio Marcilio Junior <wilson.marciliojr@g.globo>
2022-11-19 17:15:46 -0300

    update version and lint code

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from pybind11 import get_cmake_dir
 
 import sys 
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 ext_modules = None 
 
@@ -24,8 +24,8 @@ if sys.platform == 'win32':
     	Pybind11Extension("_cluster_shapley",
     		["src/cpp/cs.cpp", "src/cpp/cs_bind.cpp"],
     		language='c++',
-    		extra_compile_args = [ '/openmp', '/DEIGEN_DONT_PARALLELIZE',  '/DINFO', '-IC:/Eigen'],
-            extra_link_args = [ '/openmp', '/DEIGEN_DONT_PARALLELIZE', '/DINFO', '-IC:/Eigen'],
+    		extra_compile_args = [ '/openmp'],
+            extra_link_args = [ '/openmp'],
     		define_macros = [('VERSION_INFO', __version__)],
     		),
 
@@ -37,8 +37,10 @@ else:
     Pybind11Extension("_cluster_shapley",
         ["src/cpp/cs.cpp", "src/cpp/cs_bind.cpp"],
         language='c++',
-        extra_compile_args = ['-O3', '-shared', '-std=c++11', '-fPIC', '-fopenmp',  '-march=native', '-DINFO'],
-        extra_link_args = ['-O3', '-shared', '-std=c++11', '-fPIC', '-fopenmp',  '-march=native', '-DINFO'],
+        # extra_compile_args = ['-O3', '-shared', '-std=c++11', '-fPIC', '-fopenmp',  '-march=native', '-DINFO'],
+        # extra_link_args = ['-O3', '-shared', '-std=c++11', '-fPIC', '-fopenmp',  '-march=native', '-DINFO'],
+        extra_compile_args = ['-O3', '-std=c++11', '-fPIC', '-fopenmp'],
+        extra_link_args = ['-O3', '-std=c++11', '-fPIC', '-fopenmp'],
         define_macros = [('VERSION_INFO', __version__)],
         ),
 
@@ -59,28 +61,7 @@ setup(
     # Currently, build_ext only provides an optional "highest supported C++
     # level" feature, but in the future it may provide more features.
     cmdclass={"build_ext": build_ext},
-    install_requires=['scipy', 'sklearn', 'numpy', 'shap', 'pybind11'],
+    install_requires=['scipy', 'sklearn', 'numpy', 'shap==0.41.0', 'pybind11'],
     packages=['dr_explainer'],
     zip_safe=False,
 )
-
-# setup(
-# 	name='cluster-shapley',
-# 	packages=['dr_explainer'],#find_packages(include=['dr_explainer']),
-# 	version=__version__,
-# 	description='Explaining dimensionality reduction using SHAP values',
-# 	long_description=long_description,
-#     long_description_content_type='text/markdown',
-# 	author='Wilson E. Marcílio-Jr',
-# 	author_email='wilson_jr@outlook.com',
-# 	url='https://github.com/wilsonjr/ClusterShapley',
-# 	license='MIT',
-# 	ext_modules=ext_modules,
-# 	extras_require={"test": "pytest"},
-# 	cmdclass={"build_ext": build_ext},
-# 	install_requires=['scipy', 'sklearn', 'numpy', 'shap', 'pybind11'],
-# 	zip_safe=False,
-# 	# setup_requires=['pytest-runner'],
-# 	# tests_require=['pytest'],
-# 	# test_suite='tests',
-# )
